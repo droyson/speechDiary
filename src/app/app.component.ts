763d6af350c884198f51ce70b7867930546f2915
Royson <roysondsilva@gmail.com>
2017-05-27 00:12:59 +0530

    Base for app
    Base structure for Angular App

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `<div class="row">
+                    <div class="col-sm-12">
+                        <h1>Speech Diary</h1>
+                    </div>
+                </div>`
+})
+export class AppComponent{
+    
+}
